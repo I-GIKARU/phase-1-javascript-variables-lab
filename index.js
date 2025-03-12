89995a2985440fa394b326befc6d0be476df6622
I-GIKARU <aizohgikaru@gmail.com>
2025-03-12 21:00:49 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+const companyName = "Scuber"; // Defined as a constant
+
+let mostProfitableNeighborhood = "Chelsea"; // Defined using let
+
+let companyCeo = "Susan Smith"; // Defined using let
